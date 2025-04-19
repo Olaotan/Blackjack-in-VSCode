25a478be0980193bf1f420e695fe83afc5f5b819
Olaotan <oeoladitan@hotmail.com>
2025-04-19 16:22:39 +0100

    code updates

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 
 const cardsEl = document.getElementById("cards-el")
 const sumEl = document.getElementById("sum-el")
+
 const startBtn = document.getElementById('start-btn')
 const newCardBtn = document.getElementById("new-btn") 
+const heading = document.getElementById("heading")
 let firstCard = Math.floor(Math.random()*11) +1
 let secondCard = Math.floor(Math.random()*11) +1
 let sum = firstCard + secondCard
 
+startBtn.addEventListener('click', function(){
+    cardsEl.textContent = "Cards: " + firstCard + ' ' + secondCard
+    sumEl.textContent = "Sum: " + sum
+})
+
+function cardLogic(){
+
+    if (sum < 21) {
+        heading.textContent = "Pick a new card"
+    } else if (sum > 21) {
+        heading.textContent = "Game Over"
+    } else {
+        heading.textContent = "You've got Blackjack!"
+    }
+
+}
